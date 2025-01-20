d5ccf5d6cc05b005637cf185cc454ad3f5ae3d21
David Konsumer <konsumer@jetboystudio.com>
2025-01-20 15:45:09 -0800

    udpate example to use latest pntr_color shape

--- cmake/Findpntr.cmake
+++ cmake/Findpntr.cmake
@@ -3,6 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     pntr
     GIT_REPOSITORY https://github.com/robloach/pntr.git
-    GIT_TAG 4e373dde963cd1a4e741dc1df6619ceadf94b9a6
+    GIT_TAG 0516337
 )
 FetchContent_MakeAvailable(pntr)
